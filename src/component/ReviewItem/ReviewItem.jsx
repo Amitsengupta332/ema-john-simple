f4426989d4d802b9243c9c5891966c86829dc04b
Amitsen11 <89931804+Amitsen11@users.noreply.github.com>
2023-04-06 19:03:28 +0600

    implementing removefromDb

--- src/component/ReviewItem/ReviewItem.jsx
+++ src/component/ReviewItem/ReviewItem.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 import './ReviewItem.css';
 
-const ReviewItem = ({product}) => {
+const ReviewItem = ({ product,handleRemoveFromCart}) => {
 
-    const {id,img,price, name , quantity} = product;
+    const { id, img, price, name, quantity } = product;
 
 
     return (
         <div className='review-item'>
-             <img src={img} alt="" />
-             <div className='review-details'>
+            <img src={img} alt="" />
+            <div className='review-details'>
                 <p className='product-title'>{name}</p>
                 <p>Price: <span className='orange-text'>${price}</span></p>
                 <p>Order Quantity: <span>{quantity}</span></p>
-             </div>
-             <button>D</button>
+            </div>
+            <button onClick={() => handleRemoveFromCart(id)} className='btn-delete'>
+                <FontAwesomeIcon className='delete-icon' icon={ faTrashAlt} />
+            </button>
         </div>
     );
 };
